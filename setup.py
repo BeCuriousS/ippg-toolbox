453f78ae2daed1a098e744ba27e930191a564b38
Matthieu Scherpf <matthieu.scherpf@tu-dresden.de>
2021-02-16 20:19:16 +0100

    added simple setup file

---
+++ setup.py
@@ -0,0 +1,5 @@
+# FIXME revise this file according to https://packaging.python.org/tutorials/packaging-projects/
+
+from setuptools import setup, find_packages
+
+setup(name='ippgtoolbox', packages=['benchmark'])
