54fdcd276a64c7dc7b225a0b32530604cb81f05e
Matthieu Scherpf <matthieu.scherpf@tu-dresden.de>
2021-12-08 20:07:19 +0100

    updated test directory structure

--- ippgtoolbox/detection/skin/deeplab/deeplab_Xception65/test/test_run_deeplab_on_single_record.sh
+++ ippgtoolbox/detection/skin/deeplab/deeplab_Xception65/test/test_run_deeplab_on_single_record.sh
@@ -3,35 +3,7 @@
 rm -rf $(pwd)/test/test_output_single_record
 
 bash run_deeplab_on_single_record.sh \
-    -s $(pwd)/test/assets/test_run_deeplab_on_single_record \
+    -s $(pwd)/../test/assets/test_run_deeplab_on_single_record \
     -d $(pwd)/test/test_output_single_record \
-    -r 500x1000 \
-    -p True \
-    -k True
-
-# rm -rf $(pwd)/test/test_output_single_record
-
-# bash run_deeplab_on_single_record.sh \
-#     -s $(pwd)/test/assets/test_run_deeplab_on_single_record \
-#     -d $(pwd)/test/test_output_single_record \
-#     -r 500x1000 \
-#     -p False \
-#     -k False
-
-# rm -rf $(pwd)/test/test_output_single_record
-
-# bash run_deeplab_on_single_record.sh \
-#     -s $(pwd)/test/assets/test_run_deeplab_on_single_record \
-#     -d $(pwd)/test/test_output_single_record \
-#     -r 500x1000 \
-#     -p True \
-#     -k False
-
-# rm -rf $(pwd)/test/test_output_single_record
-
-# bash run_deeplab_on_single_record.sh \
-#     -s $(pwd)/test/assets/test_run_deeplab_on_single_record \
-#     -d $(pwd)/test/test_output_single_record \
-#     -r 500x1000 \
-#     -p False \
-#     -k True
+    -r 640x420 \
+    -t 0.8
